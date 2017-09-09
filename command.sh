78451d48defbd0bd0ae7205408ddd05e651c6c52
Andrew Ross <andrew.ross.mail@gmail.com>
2017-09-09 15:04:02 -0400

    add dropout to decoder

--- command.sh
+++ command.sh
@@ -1,3 +1,3 @@
 cp -r /code/* /output
 cd /output
-/run_jupyter.sh --no-browser --NotebookApp.base_url='/notebooks/cVsEE2mutTBHUabTXjA7U2' --NotebookApp.token='' --NotebookApp.allow_origin='*'
+/run_jupyter.sh --no-browser --NotebookApp.base_url='/notebooks/NEHorZ9F62Eawuv26XXFES' --NotebookApp.token='' --NotebookApp.allow_origin='*'
